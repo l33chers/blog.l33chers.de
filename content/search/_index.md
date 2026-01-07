626a7250a15227eb411f178b65d4f8a851606d71
Mag1cByt3s <ppeinecke@protonmail.com>
2026-01-07 20:06:26 +0100

    initial push

---
+++ content/search/_index.md
@@ -0,0 +1,6 @@
++++
+title = "Search"
+layout = "search"
++++
+
+Type a query to search across posts and pages.
---
+++ flake.nix
@@ -0,0 +1,32 @@
+{
+  description = "blog.l33chers.de";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+  outputs =
+    { self, nixpkgs }:
+    let
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+      ];
+      forAllSystems = f: nixpkgs.lib.genAttrs systems (system: f system);
+    in
+    {
+      devShells = forAllSystems (
+        system:
+        let
+          pkgs = import nixpkgs { inherit system; };
+        in
+        {
+          default = pkgs.mkShell {
+            packages = [
+              pkgs.hugo
+            ];
+          };
+        }
+      );
+    };
+}
